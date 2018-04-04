a0d9c79ed0b4ef6c43e92a38eb32beaa868c432c
Misha Behersky <bmwant@gmail.com>
2018-04-04 17:38:24 +0300

    Add fetcher

--- cli.py
+++ cli.py
@@ -1,4 +1,8 @@
+import asyncio
+
 import click
+
+from fetcher import SimpleFetcher
 
 
 @click.group()
@@ -6,7 +10,22 @@ def cli():
     pass
 
 
-def grab():
+async def grab_handler():
     """
     Update database with new set of emoji if any.
     """
+    url = 'https://www.webpagefx.com/tools/emoji-cheat-sheet/'
+    fetcher = SimpleFetcher(url=url)
+    html = await fetcher.request()
+    # print(html)
+    await fetcher.close()
+
+
+@cli.command()
+def grab():
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(grab_handler())
+
+
+if __name__ == '__main__':
+    cli()
--- utils.py
+++ utils.py
@@ -1,4 +1,21 @@
+import logging
+import coloredlogs
 
 
-def get_logger(name):
-    pass
+FORMAT = '[%(name)s] %(levelname)s:%(message)s'
+FORMATTER = logging.Formatter(fmt=FORMAT)
+
+
+def get_logger(name='default', level=logging.DEBUG):
+    logger = logging.getLogger(name)
+    logger.propagate = False
+    logger.setLevel(level)
+
+    if not logger.handlers:
+        handler = logging.StreamHandler()
+        handler.setFormatter(fmt=FORMATTER)
+        logger.addHandler(handler)
+
+    coloredlogs.install(level=level, logger=logger, fmt=FORMAT)
+
+    return logger
